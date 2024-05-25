f38c993af7e978151aa3e69dafd4a26c58ab5a97
Gagloev Kirill <kirogagl@gmail.com>
2024-05-26 00:16:02 +0300

    PlayList

---
+++ WebApplication1/Areas/PlayList/Controllers/PlayListController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Areas.PlayList.Controllers;
+public class PlayListController : Controller
+{
+    // GET: PlayListController
+    [Area("PlayList")]
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+}
