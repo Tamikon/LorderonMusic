a73b7daa8bb7d2a258f40741cf52a7941da06bc0
Saba Bestaev <bestaev.saba@gmail.com>
2024-06-07 16:49:25 +0300

    удалил не нужное свойство Id в классе User

--- DatabaseService/Models/Server.cs
+++ DatabaseService/Models/Server.cs
@@ -6,11 +6,13 @@ namespace DatabaseService.Models
     {
         [Key]
         public int Id { get; set; }
-        public string DiscordServerId { get; set; }
         public string Name { get; set; }
         public string AvatarUrl { get; set; }
-        public List<Playlist> Playlists { get; set; }
-        public int UserId { get; set; }
+        public string DiscordServerId { get; set; }
+
+        public string UserId { get; set; } 
         public User User { get; set; }
+
+        public List<Playlist> Playlists { get; set; }
     }
 }
